c7debb2f6d98ed9be558ca8e516b3d7b2f66acd5
Jopsy Varghese <jopsyvarghese@gmail.com>
2024-06-23 21:00:05 +0530

    Token based authentication and JWT implementation

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
 const express = require('express');
-const basicAuth = require('express-basic-auth');
+const db = require('./db');
+const guestRoute = require('./routes/guestRoute');
+const authRoute = require('./routes/authRoute');
+const apiRoute = require('./routes/apiRoute');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const ejs = require('ejs');
 
 const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
-// admin
-// user
-// super-user
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use('/admin', authorization, AdminPath);
-app.use('/user', UserPath);
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false } // true
+  }));
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
-})
+app.use('/protected', authRoute);
+
+app.use('/api', apiRoute);
+
+app.use('/', guestRoute);
+
+app.listen(PORT, ()=> {
+console.log(`Server running on Port ${PORT}`);
+});
---
+++ routes/apiRoute.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/api/login', async (req, res) => {
+    const {username, password} = req.body;
+    const [rows] = await db.query("SELECT id, username, user_password FROM users WHERE username=?", [username]);
+    if(!rows[0] || !rows[0].id) {
+        res.json({message: 'Invalid username or password given'});
+        return;
+    }
+    const isPasswordMatching = await bcrypt.compare(password, rows[0].user_password);
+    if(!isPasswordMatching) {
+        res.json({message: 'Invalid username or password given'});
+        return;
+    } else {
+        /**
+         * JSON Web Token (JWT)
+         * Compact
+         * Self contained
+         * Secure
+         * 
+         * 
+         * header.payload.signature
+         * 
+         * 
+         * sub, exp, iat
+         * 
+         * secret key -- HMAC algorithm
+         * private key -- RSA or ECDSA algorithm
+         * 
+         * 
+         */
+        const payload = {
+            userId: rows[0].id,
+            username: username,
+        };
+
+        const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, {expiresIn: '30m'});
+        res.json({message: 'Success', token});
+    }
+});
+
+router.get('/api/verify_token', (req, res) => {
+    const authHeader = req.headers.authorization;
+    if(!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({message: "User not authenticated"});
+    }
+    const token = authHeader.split(' ')[1];
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        return res.json({decoded});
+    } catch(err) {
+        return res.status(401).json({message: "Invalid token"});
+    }
+});
+
+module.exports = router;
